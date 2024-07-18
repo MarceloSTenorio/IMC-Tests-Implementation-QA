d59312045d87b7d8b1e10363b949b953c05fa3dd
MarceloSTenorio <marcelosouzatenorio@gmail.com>
2024-07-18 02:07:50 -0300

    -Fix  - Removed unused parameter from cypress.config.js.

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "cah3g3",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents() {},
   },
 });
